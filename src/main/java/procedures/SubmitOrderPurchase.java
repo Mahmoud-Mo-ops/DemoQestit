a23dbd496b99095d91a51fd59cf98c507d9248f3
Mahmoud Gomaa <Mahmoud.Gomaa@Qcentriseg.com>
2024-12-30 17:54:05 +0200

    make submoitorder sequence chaining

--- src/main/java/procedures/SubmitOrderPurchase.java
+++ src/main/java/procedures/SubmitOrderPurchase.java
@@ -27,16 +27,25 @@ public class SubmitOrderPurchase extends BaseTest {
 		initialize();
 	}
 
+    public void performCompleteOrderProcess(String firstName, String lastName, String postalCode) throws IOException {
+        LogIn();
+        addItemsLessThanTenDollarsToCart();
+        NavigateToCheckOutReview();
+        fillOutDataUser(firstName, lastName, postalCode);
+        gotToCompletePage();
+    }
+
 	// log in (landing page)
-	public void LogIn() {
+	public SubmitOrderPurchase LogIn() {
 		LandingPage landingPage = new LandingPage(driver);
 		landingPage.sendKeysUserNameTextField("standard_user");
 		landingPage.sendKeysPasswordTextField("secret_sauce");
 		landingPage.clickLoginButton();
+		return this;
 	}
 
 	// find all products with price less than 10 dollar
-	public void addItemsLessThanTenDollarsToCart() {
+	public SubmitOrderPurchase addItemsLessThanTenDollarsToCart() {
 		ProductCataloguePage productCataloguePage = new ProductCataloguePage(driver);
 		List<WebElement> carts = productCataloguePage.getProductCarts();
 		List<WebElement> prices = productCataloguePage.getProductPrices();
@@ -50,28 +59,32 @@ public class SubmitOrderPurchase extends BaseTest {
 			}
 		}
 		productCataloguePage.goToCart().click();
+		return this;
 
 	}
 
 	// navigate to checkout overview page //cart Page
-	public void NavigateToCheckOutReview() {
+	public SubmitOrderPurchase NavigateToCheckOutReview() {
 		CartPage cartPage = new CartPage(driver);
 		cartPage.getCheckoutButton().click();
+		return this;
 	}
 
 	// fill out data in the checkout page
 
-	public void fillOutDataUser(String firstName, String lastName, String postalCode) {
+	public SubmitOrderPurchase fillOutDataUser(String firstName, String lastName, String postalCode) {
 		CheckoutPage checkoutPage = new CheckoutPage();
 		checkoutPage.sendFirstNameTextField(firstName).sendLastNameTextField(lastName)
 				.sendPostalCodeTextField(postalCode).clickOnContinueButton();
+		return this;
 	}
 
 	// check out overview
 
-	public static void gotToCompletePage() {
+	public  SubmitOrderPurchase gotToCompletePage() {
 		CheckoutOverviewPage checkoutOverview = new CheckoutOverviewPage(GlobalVariables.getDriver());
 		checkoutOverview.getFinishButton().click();
+		return this;
 	}
 
 	// complete page
--- src/main/java/tests/SubmitOrderTest.java
+++ src/main/java/tests/SubmitOrderTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import procedures.SubmitOrderPurchase;
@@ -11,24 +12,30 @@ import utils.ConfigReader;
 import utils.GlobalVariables;
 
 public class SubmitOrderTest extends BaseTest {
-	SubmitOrderPurchase submitOrderPurchase;
-	ConfigReader configReader;
+    SubmitOrderPurchase submitOrderPurchase;
+    ConfigReader configReader;
+    WebDriver driver;
 
-	@Test
-	public void submitOrderTest() throws IOException {
-		WebDriver driver = GlobalVariables.getDriver();
-		configReader = new ConfigReader();
-		driver = GlobalVariables.getDriver();
-		driver.get(configReader.getUrl());
-		submitOrderPurchase = new SubmitOrderPurchase(driver);
-		// submitOrderPurchase.initialize();
-		submitOrderPurchase.LogIn();
-		submitOrderPurchase.addItemsLessThanTenDollarsToCart();
-		submitOrderPurchase.NavigateToCheckOutReview();
-		submitOrderPurchase.fillOutDataUser("firstName", "lastName", "postalcode");
-		SubmitOrderPurchase.gotToCompletePage();
-		String confirmationText = submitOrderPurchase.extractConfirmationText();
-       Assert.assertEquals(confirmationText, "Thank you for your order!");
+    @BeforeMethod
+    public void setup() throws IOException {
+        driver = GlobalVariables.getDriver();
+        configReader = new ConfigReader();
+        driver.get(configReader.getUrl());
+        submitOrderPurchase = new SubmitOrderPurchase(driver);
+    }
 
-	}
+    @Test
+    public void submitOrderTest() throws IOException {
+        String confirmationText = submitOrderPurchase
+            .LogIn()
+            .addItemsLessThanTenDollarsToCart()
+            .NavigateToCheckOutReview()
+            .fillOutDataUser("firstName", "lastName", "postalCode")
+            .gotToCompletePage()
+            .extractConfirmationText();
+
+        Assert.assertEquals(confirmationText, "Thank you for your order!");
+    }
+    
+    
 }
